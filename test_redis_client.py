f91b735623a2b4cb2acba9d3b32432df6dd6220f
Christian Teijon <christian.teijon@rightscale.com>
2018-08-05 19:33:02 +0200

    Fix name and add comments

--- test_redis_client.py
+++ test_redis_client.py
@@ -1,7 +1,12 @@
 import unittest
 import redis_client
 
-class TestRedis(unittest.TestCase):
+# This needs a Redis Server running
+# with Docker, you can do:
+#   docker run -d -p 6379:6379 redis
+#   REDIS_HOST=127.0.0.1 python test_redis_client.py
+
+class TestRedisClient(unittest.TestCase):
     def test_set_get(self):
         redis_client.set('my_key', 'my_value')
         self.assertEqual(redis_client.get('my_key'), 'my_value')

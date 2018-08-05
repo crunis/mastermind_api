4c3666d39233f2e1261c9bcf888f2da6a282fbee
Christian Teijon <christian.teijon@rightscale.com>
2018-08-05 20:12:10 +0200

    Add .gitignore

--- redis_client.py
+++ redis_client.py
@@ -1,6 +1,7 @@
 import redis
 import os
 import logging
+import json
 
 EXPIRES = 3600 * 24 * 7 # Automatically purge games in a week
 client = None
@@ -10,14 +11,22 @@ def rc():
     if client:
         logging.info("Reusing existing Redis client")
         return client
+
     logging.info("Creating new Redis client")
     redis_host = os.getenv('REDIS_HOST', 'redis')
     redis_port = int(os.getenv('REDIS_PORT', '6379'))
     client =  redis.StrictRedis(host=redis_host, port=redis_port, db=0)
+
     return client
 
+
 def set(key, value):
-    rc().set(key, value, EXPIRES)
+    rc().set(key, json.dumps(value), EXPIRES)
+
 
 def get(key):
-    return rc().get(key)
+    v = rc().get(key)
+    if v:
+        v = json.loads(v)
+
+    return v
